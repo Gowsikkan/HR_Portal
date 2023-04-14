38b16047b11c2a709ed0f824420623c8c4ec2c87
Gowsikkan <66643168+Gowsikkan@users.noreply.github.com>
2023-04-14 10:24:19 +0530

    initial commit

--- Registration/models.py
+++ Registration/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Department(models.Model):
+    name = models.TextField() 
+    description = models.TextField() 
+    status = models.IntegerField() 
+    date_added = models.DateTimeField(default=timezone.now) 
+    date_updated = models.DateTimeField(auto_now=True) 
+    
--- Registration/views.py
+++ Registration/views.py
@@ -9,26 +9,31 @@ DATABASE_HOST = 'localhost'
 DATABASE_PORT = '27017'
 db_handle, mongo_client = get_db_handle(DATABASE_NAME, DATABASE_HOST, DATABASE_PORT)
 collection = db_handle['login']
+dir = db_handle['Emp_directory']
 
 # Create your views here.
 def home(request):
     return render(request,'register.html')
-
     
 def login(request):
     email = request.POST['email']
     password = request.POST['password']
     dic = {}
     cursor = collection.find({},{"email": 1,"password":1})
+    
+    details = dir.find({},{"Email": 1,"Role":1,"Name":1,"Phone_no":1})
     for i in cursor:
         dic[i['email']] = i['password']
     if email in dic.keys() and password in dic.values():
-        return render(request,'profile.html')
+        return render(request,'profile.html',{'details': details})
     elif email in dic.keys() and password not in dic.values():
         return render(request,'register.html',{'message': 'Incorrect Password'})
     else:
         return render(request,'register.html',{'message': 'Incorrect Email and Password'})
     
+def emp_directory(request): 
+    cursor = dir.find({},{"Email": 1,"Role":1,"Name":1,"Phone_no":1})
+    return render (request,'directory,html',cursor)
 
     
 
